36880e2c9c9e2b4f422ff014d6a3b147f65e61a2
Pablo Pareja <pabloignaciopv@gmail.com>
2026-05-05 21:04:34 -0400

    feat: configurar React Query y providers en main.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,4 @@
-import { useEffect } from 'react'
-import { tmdbClient } from '@/services/tmdb'
-
 function App() {
-  useEffect(() => {
-    tmdbClient.get('/movie/popular')
-      .then(res => console.log(res.data))
-      .catch(err => console.error(err))
-  }, [])
-
   return (
     <div className="min-h-screen bg-gray-950 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-white">
